5bbec52646b4661eb8af239c718173a480c22edd
IHB95 <i.holguinballesteros@gmail.com>
2022-02-08 00:44:54 +0100

    08/02/2022 2 TODO añadidos

--- app/src/main/java/com/example/jugarquienquieresermillonario/TODO.kt
+++ app/src/main/java/com/example/jugarquienquieresermillonario/TODO.kt
@@ -2,6 +2,8 @@ package com.example.jugarquienquieresermillonario
 
 //MEJORAS IMPRESCINDIBLES
     //TODO("Ocultar hint y mostrar text en activity_login.xml")
+    //TODO("Identificar al usuario por el Id")
+    //TODO("Buscar query para obtener Id a través del token")
 
 //MEJORAS ADICIONALES
     //TODO ("Botón "Salir"")
